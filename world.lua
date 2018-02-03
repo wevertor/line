c28513b46ffb56899ffb8b4b30cba8f7694a24f0
Weverton Trindade <weverton2trindade@gmail.com>
2018-02-03 02:55:51 -0300

    Update world.lua
    Foi criado paredes e blocos. 
    O metro foi reajustado para 32 pixels, porém, a gravidade se comporta melhor levando em conta o metro como 64.

--- world.lua
+++ world.lua
@@ -5,8 +5,8 @@ function worldLoad( ... )
 		xy = componente y da gravidade
 		sleep = permissão para os corpos "dormirem"
 	--]]
-	love.physics.setMeter(64);							--define quanto é 1 metro no mundo
-	world = love.physics.newWorld(0, 9.81*64, true);	--cria um mundo onde a componente horizontal de gravidade é 0 e a vertical é 9.81
+	love.physics.setMeter(32);							--define quanto é 1 metro no mundo
+	world = love.physics.newWorld(0, 9.81*64, false);	--cria um mundo onde a componente horizontal de gravidade é 0 e a vertical é 9.81
 	width, height = love.graphics.getDimensions();		--atribui a variáveis as dimensões de tela 
 	wall = {};
 
@@ -15,8 +15,15 @@ function worldLoad( ... )
 	objects[1].x = width/2; 															--componente x do objects[1]
 	objects[1].y= height/2;																--componente y do objects[1]
 	objects[1].body = love.physics.newBody(world, objects[1].x, objects[1].y, "dynamic"); 	--cria um corpo para o objects[1] usando seu x e y
-	objects[1].shape = love.physics.newRectangleShape(0, 0, 30, 30);	--atribui uma forma ao objects[1]
+	objects[1].shape = love.physics.newRectangleShape(0, 0, 32, 64);	--atribui uma forma ao objects[1]
 	objects[1].fix = love.physics.newFixture(objects[1].body, objects[1].shape);			--liga a forma do objects[1] ao seu corpo
+
+	objects[2] = {};
+	objects[2].x = width/2-32; 															--componente x do objects[2]
+	objects[2].y= height/2;																--componente y do objects[2]
+	objects[2].body = love.physics.newBody(world, objects[2].x, objects[2].y, "dynamic"); 	--cria um corpo para o objects[2] usando seu x e y
+	objects[2].shape = love.physics.newRectangleShape(0, 0, 32, 32);	--atribui uma forma ao objects[2]
+	objects[2].fix = love.physics.newFixture(objects[2].body, objects[2].shape);			--liga a forma do objects[2] ao seu corpo
 
 	wall[1] = {};
 	wall[1].x = width/2;
@@ -44,6 +51,15 @@ function worldLoad( ... )
 	wall[3].body = love.physics.newBody(world, wall[3].x, wall[3].y, "static");
 	wall[3].shape = love.physics.newRectangleShape(0, 0, wall[3].width, wall[3].height);
 	wall[3].fix = love.physics.newFixture(wall[3].body, wall[3].shape);
+
+	wall[4] = {};
+	wall[4].x = width/2;
+	wall[4].y = 10;
+	wall[4].width = width;
+	wall[4].height = 10;
+	wall[4].body = love.physics.newBody(world, wall[4].x, wall[4].y, "static");
+	wall[4].shape = love.physics.newRectangleShape(0, 0, wall[4].width, wall[4].height);
+	wall[4].fix = love.physics.newFixture(wall[4].body, wall[4].shape);
 	
 end
 
@@ -56,15 +72,9 @@ function worldDraw( ... )
 	love.graphics.polygon("line", wall[1].body:getWorldPoints(wall[1].shape:getPoints()));
 	love.graphics.polygon("line", wall[2].body:getWorldPoints(wall[2].shape:getPoints()));
 	love.graphics.polygon("line", wall[3].body:getWorldPoints(wall[3].shape:getPoints()));
-	love.graphics.polygon("line", wall[3].body:getWorldPoints(wall[3].shape:getPoints()));
+	love.graphics.polygon("line", wall[4].body:getWorldPoints(wall[4].shape:getPoints()));
 	love.graphics.polygon("line", objects[1].body:getWorldPoints(objects[1].shape:getPoints()));
+	love.graphics.polygon("line", objects[2].body:getWorldPoints(objects[2].shape:getPoints()));
 end
 
 
-
-
---[[
-	-> É NECESSÁRIO CRIAR AS PAREDES LATERAIS DO MUNDO
-	-> CRIAR OS BLOCOS NO CHÃO
-
-]]
